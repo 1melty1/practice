5062168038bb60ef5ff53135b5fa32e915f05de3
Wyw <53013464+1melty1@users.noreply.github.com>
2021-07-07 01:43:27 +0800

    Update 3-2-UVa1586.cpp

--- 3-2-UVa1586.cpp
+++ 3-2-UVa1586.cpp
@@ -4,22 +4,32 @@ using namespace std;
 
 int main(){
     string formula;
-    float paw=0, total=0;
-    int number;
-    cin >> formula;
-    for (int i=0; i<formula.length()+1; i++){ //need to +1 
-    
-    if (not isdigit(formula[i]) ){
-        total += paw*number;
-        if(formula[i]=='C') paw = 12.01;
-        if(formula[i]=='H') paw = 1.008;
-        if(formula[i]=='O') paw = 16.00;
-        if (formula[i]=='N') paw = 14.01;
+    float paw, total;
+    int number, temp_number, multi;
+    while(cin >> formula){
+        paw = 0;
+        total= 0;
+        multi = 1;
         number = 1;
-    } 
-    else {
-        number = (int)formula[i] - 48;
+        temp_number = 0;
+        for (int i=formula.length()+1; i>=0; i--){
+            if (not isdigit(formula[i]) ){
+                if(formula[i]=='C') paw = 12.01;
+                if(formula[i]=='H') paw = 1.008;
+                if(formula[i]=='O') paw = 16.00;
+                if (formula[i]=='N') paw = 14.01;
+                total += paw*number;
+                number = 1;
+                multi = 1;
+                temp_number = 0;
+            } 
+            else {
+                temp_number = (int)formula[i] - 48;
+                if(multi == 10) number = number + temp_number * multi;
+                else number = temp_number;
+                multi = 10;
+            }
+        }
+    if(total != 0) printf("%.3f \n",total);
     }
-    }
-    cout << total;
 }
