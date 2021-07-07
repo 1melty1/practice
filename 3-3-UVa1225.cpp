e6b3082b9d7e9fd7e50668bf21b2ac6abe84eca7
Wyw <53013464+1melty1@users.noreply.github.com>
2021-07-07 22:19:50 +0800

    Update 3-3-UVa1225.cpp

--- 3-3-UVa1225.cpp
+++ 3-3-UVa1225.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main(){
     int arr[10];
-    string input;
-    cin >> input;
-    memset(arr, 0, sizeof(arr));
-    for (int i=0; i<input.size(); i++){
-        arr[(int)input[i] - 48] ++;
+    int input, input_times;
+    string current;
+    cin >> input_times;
+    for (int j=1 ; j<=input_times; j++){
+        cin >> input;
+        memset(arr, 0, sizeof(arr));
+        for (int i=1; i<=input; i++){
+            current = to_string(i);
+            for (int z=0; z<current.size(); z++) arr[current[z] - '0']++;
+        }
+
+    for (int i=0; i<9; i++) cout << arr[i] << ' ';
+    cout << arr[9] << '\n';
     }
-    for (int i=0; i<10; i++) cout << arr[i];
 }
