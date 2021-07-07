0b4a5f338625ac088d2561a9411cbbe65394b312
Wyw <53013464+1melty1@users.noreply.github.com>
2021-07-07 19:05:54 +0800

    Update 3-1-UVa1585.cpp

--- 3-1-UVa1585.cpp
+++ 3-1-UVa1585.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main(){
     string scorestr;
-    int streak=0, total=0;
-    cin >> scorestr;
-    for (int i=0; i<79; i++){
-        if (scorestr[i]=='O'){streak++; total+=streak;}
-        else (streak = 0);
+    int input_times, streak, total;
+    cin >> input_times;
+    for (int j=1; j <= input_times; j++){
+        streak = total = 0; //reset
+        cin >> scorestr;
+        for (int i=0; i<scorestr.length(); i++){ //不能 i<=79 不然會超出 scorestr index 範圍
+            if (scorestr[i]=='O'){streak++; total+=streak;}
+            else (streak = 0);
+        }
+        cout << total << '\n';
     }
-    cout << total;
 }
